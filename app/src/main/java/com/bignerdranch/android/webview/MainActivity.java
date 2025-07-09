3c6a06da4ddb42d1d2bdc8d930ae192b8c69962b
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-07-09 11:20:22 +0500

    v1 finished

--- app/src/main/java/com/bignerdranch/android/webview/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/webview/MainActivity.java
@@ -1,14 +1,19 @@
 package com.bignerdranch.android.webview;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.OnBackPressedCallback;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient()); //prevents opening in browser
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setDomStorageEnabled(true); // For modern HTML5 storage
+        webSettings.setDatabaseEnabled(true);   // Optional, for legacy localStorage
+        webSettings.setJavaScriptEnabled(true); // Only if site needs JS
+
+// Enable smart caching behavior
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT); // Uses cache if valid
+
+
+        webView.loadUrl("https://ais.ajou.uz/en");
+
+        this.getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    finish(); // or requireActivity().finish() in a fragment
+                }
+            }
+        });
+
     }
+
+
+
 }
